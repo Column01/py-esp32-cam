d4166aadd9d29fae29b9db5974a4d91767838d39
Column01 <colinandress999@gmail.com>
2021-11-08 14:26:35 -0500

    Add basic flask stream of feed

--- cam.py
+++ cam.py
@@ -1,4 +1,5 @@
 from threading import Thread
+import numpy as np
 
 import cv2
 
@@ -9,6 +10,7 @@ class ESP32SecurityCam(Thread):
         self.frontal = cv2.CascadeClassifier('models/frontalface.xml')
         self.profile = cv2.CascadeClassifier('models/profileface.xml')
         self.body = cv2.CascadeClassifier('models/fullbody.xml')
+        self.cur_frame = None
         Thread.__init__(self)
 
     def run(self):
@@ -17,22 +19,27 @@ class ESP32SecurityCam(Thread):
             while True:
                 ret, frame = self.cap.read()
                 gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                detections = self.body.detectMultiScale(gray, 1.1, 5)
-                if len(detections) == 0:
-                    detections = self.frontal.detectMultiScale(gray, 1.1, 5)
-                if len(detections) == 0:
-                    detections = self.profile.detectMultiScale(gray, 1.1, 5)
+                detections = self.frontal.detectMultiScale(gray, 1.3, 5)
+                # if len(detections) == 0:
+                #     detections = self.frontal.detectMultiScale(gray, 1.1, 5)
+                # if len(detections) == 0:
+                #     detections = self.profile.detectMultiScale(gray, 1.1, 5)
                 for (x, y, w, h) in detections:
                     # display the detected boxes in the colour picture
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-                cv2.imshow("Camera", frame)  
+                cv2.imshow("Camera", frame)
+                self.cur_frame = np.copy(frame)
 
                 k = cv2.waitKey(1) & 0xFF
                 if k == 27:  # esc key ends process
                     self.cap.release()
                     break
             cv2.destroyAllWindows()
+    
+    def get_frame(self):
+        ret, jpeg = cv2.imencode('.jpg', self.cur_frame)
+        return jpeg.tobytes()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from cam import ESP32SecurityCam
+from flask import Flask, Response, render_template
+
+app = Flask("Camera Feeds")
+cam_threads = []
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/stream")
+def stream():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen():
+    while True:
+        #get camera frame
+        frame = cam_threads[0].get_frame()
+        if frame is None:
+            raise Exception("The frame could not be found!")
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 if __name__ == "__main__":
-    cam_threads = []
-
     cam = ESP32SecurityCam("http://192.168.0.27:81/stream")
     cam.start()
     cam_threads.append(cam)
+    app.run("0.0.0.0", port=5000)
